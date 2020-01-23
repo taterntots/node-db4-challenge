8b4bdb71a65d3a4c54b2cad4d581d3e9f22d6c64
taterntots <matthew.a.weidner@gmail.com>
2020-01-23 17:45:24 -0500

    getShoppingList() function is working and returns what it needs to.

--- routers/recipeModel.js
+++ routers/recipeModel.js
@@ -1,10 +1,32 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-  getRecipes
+  getRecipes,
+  getRecipeById,
+  getShoppingList
 }
 
 function getRecipes() {
   return db('recipes');
   // db.select('*').from('schemes') // does the same thing
+}
+
+function getRecipeById(recipe_id) {
+  return db('recipes')
+    .where({ id: recipe_id })
+    .first();
+}
+
+function getShoppingList(recipe_id) {
+  return db
+  .select(
+    'recipes.recipe_name',
+    'recipe_ingredients.ingredient_quantity',
+    'recipe_ingredients.quantity_units',
+    'ingredients.ingredient_name'
+  )
+  .from('recipes')
+  .join('recipe_ingredients', 'recipes.id', 'recipe_ingredients.recipe_id')
+  .join('ingredients', 'ingredients.id', 'recipe_ingredients.ingredient_id')
+  .where('recipes.id', '=', recipe_id)
 }
--- routers/recipeRouter.js
+++ routers/recipeRouter.js
@@ -9,7 +9,35 @@ router.get('/', (req, res) => {
       res.status(200).json(recipes);
     })
     .catch(error => {
-      res.status(500).json({ errorMessage: 'There was an error retrieving the list of recipes' })
+      res.status(500).json({ errorMessage: 'There was an error retrieving the list of recipes' });
+    })
+})
+
+//get a specific recipe
+router.get('/:id/', (req, res) => {
+  const { id } = req.params;
+
+  Recipes.getRecipeById(id)
+    .then(recipe => {
+      if (recipe) {
+        res.json(recipe);
+      } else {
+        res.status(404).json({ message: 'Could not find scheme with given id' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get specified recipe' });
+    })
+})
+
+//
+router.get('/:id/shoppinglist', (req, res) => {
+  Recipes.getShoppingList(req.params.id)
+    .then(recipe => {
+        res.status(200).json(recipe);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get specified ingredients for shopping list' });
     })
 })
 
