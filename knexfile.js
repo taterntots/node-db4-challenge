237a4c633605fa7ee5b7d3378c76f27cf1c6196b
taterntots <matthew.a.weidner@gmail.com>
2020-01-23 15:47:34 -0500

    created my tables in my migration files

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,13 @@ module.exports = {
     },
     seeds: {
       directory: './data/seeds'
+    },
+    // SQLite will not enforce foreign key constraints by default
+    // ONLY NEEDED FOR SQLITE
+    pool: {
+      afterCreate: (conn, done) => {
+            conn.run("PRAGMA foreign_keys = ON", done); // tur on foreign key enforcement
+      }
     }
   }
 
